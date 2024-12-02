4ebd29905307b2e0db0d54c2f6a9c18e202dab13
Eojin <raeojin@gmail.com>
2024-12-02 22:19:41 +0900

    add google adsense script

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ import os
 
 
 def main():
+
+    # Google AdSense script
+    st.markdown("""
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5826065978859520"
+     crossorigin="anonymous"></script>
+    """, unsafe_allow_html=True)
         
 
     # Page Config
